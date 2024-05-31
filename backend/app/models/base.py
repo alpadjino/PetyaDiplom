b34400bf3fb5174dcc710926fe5d3146aa59927d
Денис Косаревский <denis.kosarevksij@tetrika.school>
2024-05-31 19:46:25 +0300

    абв

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel as PydanticBaseModel
 
 
 class BaseModel(Document):
-    def to_json(self, obj: dict | PydanticBaseModel = None) -> dict:
+    def _to_json(self, obj: dict | PydanticBaseModel = None) -> dict:
         if isinstance(obj, PydanticBaseModel):
             obj = obj.model_dump()
         model_dump = obj or self.model_dump()
